ed31d832c8feceb899b6fdfab3525c6116f99d51
Nikeuk <nikeuk@gmail.com>
2016-02-23 21:18:41 +0300

    Initial arduino server
    initial

---
+++ Server.js
@@ -0,0 +1,27 @@
+/**
+ * Created by nikeuk on 23.02.16.
+ */
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongo = require('mongojs');
+
+var app = express();
+app.use(bodyParser.json());
+app.get('/', function(req, res){
+    console.log('i received the get request');
+    res.status(200).send('Ok');
+});
+app.get('/test', function(req, res){
+   console.log('i received the get request to test');
+    res.status(200).send('>Hello Cruel World<');
+});
+app.post('/test', function(req, res){
+    console.log('I received the POST request');
+    var data = req.body;
+    data[mData] = new date();
+    console.log(data);
+    res.status(200).send('Post test');
+});
+
+app.listen(3000);
+console.log('Server rinning on port 3000');
